c8451d2418b0b84e7eb1573a767b3ca174435e87
samyszz <samyraalves20007@gmail.com>
2026-05-01 01:52:40 -0300

    fix: ajustes de build e caminhos de arquivos

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 1600,
     rollupOptions: {
-      // Se o log indicar um módulo específico, adicione-o aqui.
-      // Exemplo: external: ['nome-do-modulo']
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
     }
   }
 })
