f3906461a6266b5613258c31a6c552c65e4791ab
samyszz <samyraalves20007@gmail.com>
2026-05-01 01:48:49 -0300

    feat: meu refúgio de memórias finalizado de vez!

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Se o log indicar um módulo específico, adicione-o aqui.
+      // Exemplo: external: ['nome-do-modulo']
+    }
+  }
 })
